4280aa3f89bf8195cb8b1da663bf64aaab0af525
wymn676-cpu <wymn676@gmail.com>
2026-03-04 08:05:43 -0600

    Delete chess_projct directory

--- chess_projct/app/src/main/java/com/antiquechess/domain/model/Game.kt
+++
@@ -1,9 +0,0 @@
-package com.antiquechess.domain.model
-
-data class Game(
-    val gameId: String,
-    val pgn: String,
-    val endTime: Long,
-    val whitePlayer: String,
-    val blackPlayer: String
-)
--- chess_projct/app/src/main/java/com/antiquechess/domain/repository/ChessRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.antiquechess.domain.repository
-
-import com.antiquechess.domain.model.Game
-
-interface ChessRepository {
-    suspend fun getRecentGames(username: String, limit: Int = 30): Result<List<Game>>
-}
